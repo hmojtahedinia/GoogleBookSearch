be5aca094ed29dd1831b1f7c34321b31e3b34c06
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-06-18 18:45:29 -0400

    first ver.

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+  <footer className="page-footer">
+    <div className="container-fluid text-center">
+      <p>&copy; Ganimed Consulting | Toronto </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
+      <div id="navbarNav">
+        <h2 id="title1" className="text-white">
+          Google Book Search
+        </h2>
+        <ul className="navbar-nav">
+          <li className="nav-item" id="home">
+            <a className="nav-link" href="/">
+              <button
+                type="button"
+                className="btn btn-info text-white  hvr-ripple-out"
+              >
+                Search Books
+              </button>
+            </a>
+          </li>
+          <li className="nav-item" id="report">
+            <a className="nav-link" href="/saved">
+              <button
+                type="button"
+                className="btn btn-warning text-white  hvr-ripple-out"
+              >
+                Saved Books
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;

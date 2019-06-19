e51a362ce99cf1c79502362055f07f5a179e2f24
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-06-19 13:43:05 -0400

    fixing deploy

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.597a58dd3a781c0f43eb3e0a20b1e052.js"
+  "/GoogleBookSearch/precache-manifest.9737bf03fa748933005d582fc7cd3c10.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/GoogleBookSearch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

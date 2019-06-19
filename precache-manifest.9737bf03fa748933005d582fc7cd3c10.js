14f44a21170e1a1aa95cb4c51ad0ff3fbc0bc616
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-06-19 14:01:17 -0400

    Updates

---
+++ precache-manifest.9737bf03fa748933005d582fc7cd3c10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c6654b98e3b34a6a35e",
+    "url": "/GoogleBookSearch/static/js/runtime~main.dfee3e5a.js"
+  },
+  {
+    "revision": "abda16660a85a0abf847",
+    "url": "/GoogleBookSearch/static/js/main.ca5b8e18.chunk.js"
+  },
+  {
+    "revision": "cebc7e89ee5f7e5aa05a",
+    "url": "/GoogleBookSearch/static/js/2.dd2871f0.chunk.js"
+  },
+  {
+    "revision": "abda16660a85a0abf847",
+    "url": "/GoogleBookSearch/static/css/main.e37b8078.chunk.css"
+  },
+  {
+    "revision": "e9957b0841d9a2ca9d30928bd2b2465f",
+    "url": "/GoogleBookSearch/index.html"
+  }
+];

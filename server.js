9301505dd30646e611caa7c1c126e55fa2f7feb3
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-06-20 17:55:45 -0400

    updating server package.json

--- server.js
+++ server.js
@@ -2,23 +2,25 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 // define connection to DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooksearch");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooksearch"
+);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
-app.use(require('./routes'))
+app.use(require("./routes"));
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -28,4 +30,5 @@ app.use(require('./routes'))
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(process.env.NODE_ENV);
 });

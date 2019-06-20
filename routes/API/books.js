7367d3f92f7a3025f255863aaf715b74cd9e8a7d
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-06-20 18:25:27 -0400

    updates

--- routes/API/books.js
+++ routes/API/books.js
@@ -4,13 +4,13 @@ const db = require("../../models");
 
 // Matches with "/api/books"
 router
-  .route("/")
+  .route("/books")
   .get(booksController.findAll)
   .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router
-  .route("/:id")
+  .route("/books/:id")
   .get(booksController.findById)
   .put(booksController.update)
   .delete(booksController.remove);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const apiRoutes = require("./API");
+const apiRoutes = require("./API/books.js");
 
 // API Routes
 router.use("/api", apiRoutes);

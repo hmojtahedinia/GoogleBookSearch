a7683c6b316ff50340efbdae65840d08fa15d92f
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-06-20 18:32:59 -0400

    build updates

---
+++ client/build/precache-manifest.a3b30544183071adc5fc34c5c0f8e6a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c6654b98e3b34a6a35e",
+    "url": "/GoogleBookSearch/static/js/runtime~main.dfee3e5a.js"
+  },
+  {
+    "revision": "a349d256c37b41b79078",
+    "url": "/GoogleBookSearch/static/js/main.b39b122b.chunk.js"
+  },
+  {
+    "revision": "cebc7e89ee5f7e5aa05a",
+    "url": "/GoogleBookSearch/static/js/2.dd2871f0.chunk.js"
+  },
+  {
+    "revision": "a349d256c37b41b79078",
+    "url": "/GoogleBookSearch/static/css/main.ed9565cd.chunk.css"
+  },
+  {
+    "revision": "c190901d24205a120a8ab03313fb4ff9",
+    "url": "/GoogleBookSearch/index.html"
+  }
+];
